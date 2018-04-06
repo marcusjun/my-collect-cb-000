68230c1b1f926c022c21b8c51de9fa4175f664ea
Marcus Jun <mjun@calstate.edu>
2018-04-06 18:17:09 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def collect (languages)
+  if block_given?
+    i=0
+    collection=[]
+    while i< languages.length
+      collection<<yield(array[i]) {|lang| lang.upcase}
+    end
+  else
+  end
+end
