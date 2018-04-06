94f69fdaaeaddb13f0e91ebaa2836e0b129b4b7c
Marcus Jun <mjun@calstate.edu>
2018-04-06 18:23:03 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect (languages)
   if block_given?
     i=0
     collection=[]
-    while i< languages.length
+    while i < languages.length
       collection<<yield(languages[i])
       i+=1
     end
